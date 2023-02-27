81621f156cb683a7ca963769e98c503f548d926f
Gerardo Lucario <Gerardo.Lucario@teck.com>
2023-02-27 16:15:11 -0600

    homework

--- src/domain.py
+++ src/domain.py
@@ -1,2 +1,12 @@
-def get_hi_for_users(users):
+# 1. Typing args en python2.7 y 3.9
+# Docs
+"""
+users: List users. per each user, concat a hi
+*args = saludos: ("hi", "hola") -> solo uno
+return ["{saludo} user.name", ]
+users is empty or args empty -> []
+"""
+def get_hi_for_users(users, *args):
+    """
+    """
     pass
--- tests/test_domain.py
+++ tests/test_domain.py
@@ -3,6 +3,23 @@ from unittest import TestCase
 from domain import get_hi_for_users
 
 class TestDomain(TestCase):
-    def test_get_hi_for_users(self):
-        result = get_hi_for_users(None)
-        self.assertEqual(result, None)
+    def test_get_hi_for_users_one_hi_success(self):
+        # Preparar tu test
+        users = [User(name="gml", age=27, is_active=True)]
+
+        # llamada de interes
+        result = get_hi_for_users(users, "hi")
+
+        # checar de lo esperado
+        self.assertEqual(result, ["hi gml"])
+
+    def test_get_hi_for_users_no_users_expected_value(self):
+        # Preparar tu test
+        users = []
+
+        # llamada de interes
+        result = get_hi_for_users(users, "hi")
+
+        # checar de lo esperado
+        self.assertEqual(result, [])
+
