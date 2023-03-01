26ffa6d8a26639f07e6feb8bc206e11db76a8056
Gerardo Lucario <Gerardo.Lucario@teck.com>
2023-02-28 20:20:47 -0600

    define user model

--- src/models/user.py
+++ src/models/user.py
@@ -1,4 +1,16 @@
+from pony.orm import Required, PrimaryKey
+
+from db import app_db
+
 class User:
     name: str
     age: int
     is_active: bool
+
+
+class UserEntity(app_db.Entity):
+    _table_ = "user"
+    user_id = PrimaryKey(int, auto=True)
+    name = Required(str, unique=True)
+    age = Required(int)
+    is_active = Required(bool)
